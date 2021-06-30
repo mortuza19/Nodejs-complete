a44a2df74b56d71320a6534153807db095ceb186
mortuza19 <sahilmortuza.90@gmail.com>
2021-06-30 20:56:11 +0530

    Added mongodb for products

--- models/product.js
+++ models/product.js
@@ -1,27 +1,41 @@
-const Sequelize = require('sequelize');
-
-const sequelize = require('../util/database');
-
-const Product = sequelize.define('product', {
-  id: {
-    type: Sequelize.INTEGER,
-    autoIncrement: true,
-    allowNull: false,
-    primaryKey: true
-  },
-  title: Sequelize.STRING,
-  price: {
-    type: Sequelize.DOUBLE,
-    allowNull: false
-  },
-  imageUrl: {
-    type: Sequelize.STRING,
-    allowNull: false
-  },
-  description: {
-    type: Sequelize.STRING,
-    allowNull: false
+const mongoDb = require('mongodb');
+const getDb = require('../util/database').getDb;
+class Product {
+  constructor(title, price, description, imageUrl, id) {
+    this.title = title;
+    this.price = price;
+    this.description = description;
+    this.imageUrl = imageUrl;
+    this._id = id;
   }
-});
+
+  save() {
+    const db = getDb();
+    if (this._id) {
+      return db.collection('products').updateOne({_id: new mongoDb.ObjectId(this._id)}, {$set: {
+        title: this.title,
+        price: this.price,
+        description: this.description,
+        imageUrl: this.imageUrl,
+      }});
+    }
+    return db.collection('products').insertOne(this);
+  }
+
+  static fetchAll() {
+    const db = getDb();
+    return db.collection('products').find().toArray();
+  }
+
+  static findById(id) {
+    const db = getDb();
+    return db.collection('products').find({_id: new mongoDb.ObjectId(id)}).next();
+  }
+
+  static deleteById(id) {
+    const db = getDb();
+    return db.collection('products').deleteOne({_id: new mongoDb.ObjectId(id)});
+  }
+}
 
 module.exports = Product;
--- util/database.js
+++ util/database.js
@@ -1,8 +1,24 @@
-const Sequelize = require('sequelize');
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
 
-const sequelize = new Sequelize('node-complete', 'root', 'nodecomplete', {
-  dialect: 'mysql',
-  host: 'localhost'
-});
+let _db;
 
-module.exports = sequelize;
+const mongoConnect = (cb) => {
+    MongoClient.connect('mongodb+srv://Sahil:Aa1cozRE7x10rZHM@cluster0.nt36a.mongodb.net/shop?retryWrites=true&w=majority')
+    .then(client => {
+        _db = client.db();
+        cb();
+    }).catch(err => console.log(err));
+}
+
+const getDb = () => {
+    if(_db) {
+        return _db;
+    }
+    throw 'No connection found!';
+}
+
+module.exports = {
+    mongoConnect: mongoConnect,
+    getDb: getDb
+};
